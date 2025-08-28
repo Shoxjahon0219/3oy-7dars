1d5a963c06017da2daabdcd886cb9c1254debcbb
Shohjahon <shoxjahonzohirov2010@gmail.com>
2025-08-28 18:11:28 +0500

    laaast

--- controllers/waterProduct.controller.js
+++ controllers/waterProduct.controller.js
@@ -2,7 +2,9 @@ const pool = require("../config/db");
 
 const getAllWaterProducts = async (req, res) => {
   try {
-    const products = await pool.query("SELECT * FROM water_products ORDER BY id");
+    const products = await pool.query(
+      "SELECT * FROM water_products ORDER BY id"
+    );
     res.status(200).send(products.rows);
   } catch (err) {
     console.log(err);
@@ -13,12 +15,15 @@ const getAllWaterProducts = async (req, res) => {
 const getWaterProductById = async (req, res) => {
   try {
     const { id } = req.params;
-    const product = await pool.query("SELECT * FROM water_products WHERE id = $1", [id]);
-    
+    const product = await pool.query(
+      "SELECT * FROM water_products WHERE id = $1",
+      [id]
+    );
+
     if (product.rows.length == 0) {
       return res.status(404).send({ error: "Water product not found" });
     }
-    
+
     res.status(200).send(product.rows[0]);
   } catch (err) {
     console.log(err);
@@ -29,7 +34,7 @@ const getWaterProductById = async (req, res) => {
 const createWaterProduct = async (req, res) => {
   try {
     const { name, volume_liters, price } = req.body;
-    
+
     const newProduct = await pool.query(
       "INSERT INTO water_products (name, volume_liters, price) VALUES ($1, $2, $3) RETURNING *",
       [name, volume_liters, price]
@@ -37,7 +42,7 @@ const createWaterProduct = async (req, res) => {
 
     res.status(201).send({
       message: "Water product created successfully",
-      data: newProduct.rows[0]
+      data: newProduct.rows[0],
     });
   } catch (err) {
     console.log(err);
@@ -49,7 +54,7 @@ const updateWaterProduct = async (req, res) => {
   try {
     const { id } = req.params;
     const { name, volume_liters, price } = req.body;
-    
+
     const updatedProduct = await pool.query(
       "UPDATE water_products SET name = $1, volume_liters = $2, price = $3 WHERE id = $4 RETURNING *",
       [name, volume_liters, price, id]
@@ -61,7 +66,7 @@ const updateWaterProduct = async (req, res) => {
 
     res.status(200).send({
       message: "Water product updated successfully",
-      data: updatedProduct.rows[0]
+      data: updatedProduct.rows[0],
     });
   } catch (err) {
     console.log(err);
@@ -72,7 +77,7 @@ const updateWaterProduct = async (req, res) => {
 const deleteWaterProduct = async (req, res) => {
   try {
     const { id } = req.params;
-    
+
     const deletedProduct = await pool.query(
       "DELETE FROM water_products WHERE id = $1 RETURNING *",
       [id]
@@ -84,7 +89,7 @@ const deleteWaterProduct = async (req, res) => {
 
     res.status(200).send({
       message: "Water product deleted successfully",
-      data: deletedProduct.rows[0]
+      data: deletedProduct.rows[0],
     });
   } catch (err) {
     console.log(err);
@@ -97,5 +102,5 @@ module.exports = {
   getWaterProductById,
   createWaterProduct,
   updateWaterProduct,
-  deleteWaterProduct
+  deleteWaterProduct,
 };
